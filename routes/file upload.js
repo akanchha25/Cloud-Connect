3dd6df254261335dc38404f52d7eba8d25d780eb
akanchha25 <75972629+akanchha25@users.noreply.github.com>
2023-01-28 18:14:49 +0530

    added view all files

---
+++ routes/file upload.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const passport = require('passport')
+const {google} = require('googleapis');
+
+const multer = require('multer')
+
+const router = express.Router()
+const path = require('path');
+
+router.get('/', function(request, response, next) {
+    response.render('fileupload', {title: 'Welcome to Cloud connect!...infinity', message : request.flash('success')})
+} );
+
+router.post('/', function(request, response, next) {
+
+    var storage = multer.diskStorage({
+        destination: function(request, file, callback)
+        {
+            callback(null, './upload');
+        },
+        filename : function(request, file, callback)
+        {
+            var temp_file_arr = file.originalname.split(".");
+            var temp_file_name = temp_file_arr[0];
+            var temp_file_extension = temp_file_arr[1];
+
+            callback(null, temp_file_name + '.' + temp_file_extension);
+        }
+    });
+
+    var upload = multer({storage: storage}).single('sample_image');
+    upload(request, response, function(error) {
+        if(error) {
+            return response.end('Error Uploading File');
+
+        }
+        else {
+            //return response.end('File Uploaded Successfully')
+            
+            /* Setting a flash message. */
+            request.flash('success', request.file.filename)
+            //console.log(file);
+            response.redirect('/fileupload')
+            console.log(request.file.filename);
+            console.log(request.file.path);
+           
+            
+        }
+    });
+
+})
+
+
+module.exports = router;
